67058d1b87e7daca7e3f282361150f2055797708
Ikramul-m <i20727172@gmail.com>
2024-04-21 15:41:58 +0530

    Weather App

--- src/Components/Weather App/WeatherApp.jsx
+++ src/Components/Weather App/WeatherApp.jsx
@@ -11,11 +11,56 @@ import wind_icon from "../Assets/wind.png";
 import humdity_icon from "../Assets/humidity.png";
 
 const WeatherApp = () => {
+  let api_key = "3ea74e4e480c02d01bc0187c8869ab25";
+
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+    let response = await fetch(url);
+
+    let data = await response.json();
+
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temperature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+
+    // humidity[0].innerHTML = data.main.humidity;
+    // wind[0].innerHTML = data.wind.speed;
+    // temperature[0].innerHTML = data.main.temp;
+    // location[0].innerHTML = data.name;
+
+    if (humidity.length > 0) {
+      humidity[0].innerHTML = data.main.humidity+" %";
+    }
+
+    if (wind.length > 0) {
+      wind[0].innerHTML = data.wind.speed+" km/hr";
+    }
+
+    if (temperature.length > 0) {
+      temperature[0].innerHTML = data.main.temp+" °C";
+    }
+
+    if (location.length > 0) {
+      location[0].innerHTML = data.name;
+    }
+  };
+
   return (
     <div className="container">
       <div className="top-bar">
         <input type="text" className="cityInput" placeholder="Search" />
-        <div className="search-icon">
+        <div
+          className="search-icon"
+          onClick={() => {
+            search();
+          }}
+        >
           <img src={search_icon} alt="" />
         </div>
       </div>
@@ -38,7 +83,7 @@ const WeatherApp = () => {
         <div className="element">
           <img src={wind_icon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percent">18 km/hr</div>
+            <div className="wind-rate">18 km/hr</div>
             <div className="text">Wind Speed</div>
           </div>
         </div>
